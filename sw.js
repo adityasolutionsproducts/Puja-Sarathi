6c595e8279b8ec2be47a17ab80d7ff865f33deaa
adityasolutionsproducts <aditya.solutions.products@gmail.com>
2026-09-16 20:54:36 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Puja Sarathi PWA Service Worker v1.0 */
-const CACHE_NAME = 'puja-sarathi-v2-2025-09-15';
+const CACHE_NAME = 'puja-sarathi-v3-2025-09-15';
 const CORE_ASSETS = [
   './',
   './index.html',
